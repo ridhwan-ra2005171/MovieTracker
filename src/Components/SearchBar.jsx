f14151927751a5431daf834d17adfdd903443d37
Ridhwan Athaullah <123949019+ridhwan-ra2005171@users.noreply.github.com>
2024-10-06 14:10:28 +0300

    New small feature on Search bar
    searchbar is main focus when user lands, and also when the user presses enter key.
    
    used useEffect, useRef( to get DOM)

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,6 +1,31 @@
-import React from "react";
+import React, {useRef, useEffect} from "react";
 
 export default function Search({query, setQuery}) {
+
+  //we want the search bar to be the main focus when the user lands
+  const inputEl = useRef(null);
+  useEffect(function(){
+
+    function callback(e) {
+
+      if(document.activeElement === inputEl.current) {
+        return; //dont reset the input
+      }
+
+      if(e.key === 'Enter') {
+        e.preventDefault();
+        inputEl.current.focus();
+        setQuery(""); //resets the input
+      }
+    }
+
+    //we want to focus on search bar when user keydown enter
+    document.addEventListener('keydown', callback)
+
+    inputEl.current.focus(); //this is correct way of calling DOM
+    //cleanup function
+    return()=> document.addEventListener('keydown', callback);
+  }, []);
 
     return(
         <input
@@ -9,6 +34,7 @@ export default function Search({query, setQuery}) {
         placeholder="Search movies..."
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        ref={inputEl}
       />
     )
 }
